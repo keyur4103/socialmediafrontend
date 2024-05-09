d10321bf5ce365c78677d2c2260e1d2ff4e9169d
keyur4103 <113822409+keyur4103@users.noreply.github.com>
2024-05-09 17:18:09 +0530

    first commit

--- src/components/Friend.jsx
+++ src/components/Friend.jsx
@@ -23,7 +23,7 @@ const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
 
   const patchFriend = async () => {
     const response = await fetch(
-      `http://localhost:6001/users/${_id}/${friendId}`,
+      `https://socialmediabackend-wr40.onrender.com/users/${_id}/${friendId}`,
       {
         method: "PATCH",
         headers: {
